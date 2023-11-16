1082929f8c0cafac0c99ca4d96745529bceb0dd5
ajuvercr <arthur.vercruysse@outlook.com>
2023-11-16 15:52:45 +0100

    add rml mapping processor to pipeline

--- generate/get_weather.js
+++ generate/get_weather.js
@@ -1,18 +1,23 @@
-export function get_weather(writer, interval) {
-  console.log("starting get_weather");
+export function get_weather(writer, interval, reader) {
+  console.log("starting get_weather", typeof interval);
+  const fn = async () => {
+    const resp = await fetch(
+      "https://data.buienradar.nl/2.0/feed/json",
+      { headers: { "Accept": "application/json" } },
+    );
+
+    const json = await resp.json();
+    const measurements = json["actual"]["stationmeasurements"];
+
+    await writer.push(JSON.stringify(json) + "\n");
+
+    console.log("Pushed ", measurements.length, "measurements");
+  };
   return () => {
-    setInterval(async () => {
-      const resp = await fetch(
-        "https://data.buienradar.nl/2.0/feed/json",
-        { headers: { "Accept": "application/json" } },
-      );
+    fn();
+    setInterval(fn, interval || 1000);
 
-      const json = await resp.json();
-      const measurements = json["actual"]["stationmeasurements"];
-
-      await writer.push(JSON.stringify(measurements) + "\n");
-
-      console.log("Pushed ", measurements.length, "measurements");
-    }, interval || 1000);
+    console.log(reader);
+    reader.end();
   };
 }
